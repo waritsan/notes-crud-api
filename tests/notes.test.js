cecb599c420473686c2714f799c5b68f23e29852
“Warit <waritsan@gmail.com>
2023-11-27 16:35:56 +0700

    add test case

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -1,1 +1,20 @@
- 
+'use strict'
+let init = require('./steps/init');
+let { an_authenticated_user } = require('./steps/given');
+let idToken;
+
+describe(`given an authenticated user`, () => {
+    beforeAll(async () => {
+        init();
+        let user = await an_authenticated_user();
+        idToken = user.AuthenticationResult.IdToken;
+        console.log(idToken);
+    });
+
+    describe(`when we invoke POST /notes endpoint`, () => {
+        it("should create a new note", async () => {
+            expect(true).toBe(true);
+        });
+    });
+});
+
