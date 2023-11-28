fafb69fbc9a9073b12bb01ca005aa9e17f37458b
“Warit <waritsan@gmail.com>
2023-11-28 16:45:17 +0700

    add create test

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -1,6 +1,7 @@
 'use strict'
 let init = require('./steps/init');
 let { an_authenticated_user } = require('./steps/given');
+let { we_invoke_createNote } = require('./steps/when');
 let idToken;
 
 describe(`given an authenticated user`, () => {
@@ -13,8 +14,14 @@ describe(`given an authenticated user`, () => {
 
     describe(`when we invoke POST /notes endpoint`, () => {
         it("should create a new note", async () => {
-            expect(true).toBe(true);
+            const body = {
+                id: '1000',
+                title: 'My test note',
+                body: 'Hello this is the note body'
+            }
+            let result = await we_invoke_createNote({ idToken, body });
+            expect(result.statusCode).toEqual(201);
+            expect(result.body).not.toBeNull();
         });
     });
 });
-
--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,7 +1,38 @@
 'use strict';
+const _ = require('lodash');
+const Promise = this.Promise || require('promise');
+const agent = require('superagent-promise')(require('superagent'), Promise);
 
-const init = async () => {
-    require('dotenv').config();
+const makeHttpRequest = async (path, method, options) => {
+    let root = process.env.TEST_ROOT;
+    let url = `${root}/${path}`;
+    let httpReq = agent(method, url);
+    let body = _.get(options, 'body');
+    let idToken = _.get(options, 'idToken');
+    console.log(`invoking HTTP ${method} ${url}`);
+
+    try {
+        // Set authorization header
+        httpReq.set('Authorization', idToken);
+        if (body) {
+            httpReq.send(body);
+        }
+        let response = await httpReq;
+        return {
+            statusCode: response.status,
+            body: response.body
+        }
+    } catch (err) {
+        return {
+            statusCode: err.status,
+            body: null,
+        };
+    }
+};
+
+exports.we_invoke_createNote = (options) => {
+    // Make an HTTP call
+    let response = makeHttpRequest('notes', 'POST', options);
+    return response;
+
 }
-
-module.exports = init;
